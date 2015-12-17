20110bdf6f8e66896907229d6248695d1e586607
Masashi NAGAO <masashi.gao@icloud.com>
2015-12-17 20:41:58 +0900

    done designing in storyboard

---
+++ DNApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DNApp
+//
+//  Created by Meng To on 2015-01-25.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
